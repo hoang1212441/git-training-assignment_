2e4ea45194677e019bead3dba91b9c63c727c073
Nguyen Huy Hoang <nguyenhuyhoang31102005@gmail.com>
2026-01-21 16:41:50 +0700

    Implement multiply operation

---
+++ calculator/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public enum SIGN {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    public static Double calculate(SIGN sign, Double arg1, Double arg2) {
+        switch (sign) {
+            case PLUS:
+                return arg1 * arg2;
+            default:
+                return 0d;
+        }
+    }
+}
